08e14728cccb087acea053314f6102e426b2994a
Ryan Riley <rriley.cs@gmail.com>
2018-09-03 11:25:11 +0300

    Adding the demo code from lec01

---
+++ lec01_2018-09-02/SyntaxTest.java
@@ -0,0 +1,16 @@
+public class SyntaxTest {
+    
+    public static void main(String[] args) { 
+        int a = 5; 
+        double b = 5.6; 
+        
+        if (a < 10) { 
+            System.out.println("Small");
+            System.out.println("Bob");
+        }
+        else
+            System.out.println("Big");
+            System.out.println("Fred");
+        System.out.println(b);
+    }
+}
---
+++ lec01_2018-09-02/syntax.py
@@ -0,0 +1,8 @@
+a = "Hi there"
+print(a)
+a = 5
+a is 23
+if a > 10:
+    This is strange
+else:
+    print("We made it!")
